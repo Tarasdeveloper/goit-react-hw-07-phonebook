83eef71cfad348953fa56a6eeccf0ffb25595e7c
Tarasdeveloper <tomahavki1@gmail.com>
2023-08-07 21:28:50 +0300

    added all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
+import ContactForm from './ContactForm/ContactForm';
+import Wrapper from './Wrapper/Wrapper';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrapper>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </Wrapper>
   );
 };
+
+export default App;
